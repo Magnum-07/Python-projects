bae5e2b96734361141d9670eb4be7fe4d51e1926
Aditya Garg <66016411+Magnum-07@users.noreply.github.com>
2020-12-06 13:29:52 +0530

    Fixed

--- turtle_mover/Random Walk.py
+++ turtle_mover/Random Walk.py
@@ -10,13 +10,13 @@ directions = [0, 90, 180, 270]
 
 # TODO 2:- Make the turtle randomly walk
 #
-# for i in range(1000):
-#     pummy.color(random.choice(colors))
-#     pummy.forward(10)
-#     pummy.setheading(random.choice(directions))
+for i in range(1000):
+    pummy.color(random.choice(colors))
+    pummy.forward(10)
+    pummy.setheading(random.choice(directions))
 
-for i in range(4):
-    pummy.forward(100)
-    pummy.setheading(90)
+# for i in range(4):
+#     pummy.forward(100)
+#     pummy.setheading(90)
 screen = Screen()
 screen.exitonclick()
